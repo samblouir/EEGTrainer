eddf84763ce289a848cf028aa6a06f1c94479453
samblouir <samblouir@gmail.com>
2021-02-13 13:03:47 -0500

    Removed env-variable requirement in run. Added pull on failure

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ sudo docker run -it --gpus 1 \
     		-v $EEGTrainer_path/data:/data \
 		-e HOST_PERMS="$(id -u):$(id -g)" \
 		--network=host \
-		--rm $ZEUS_DOCKER_NAME:$ZEUS_DOCKER_TAG python $EEGTrainer_path/Python/tf \
+		--rm zeusids/zeusids python $EEGTrainer_path/Python/tf \
 		|| exit 1
 
 exit 0		
